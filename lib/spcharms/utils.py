820370a7f3a1f8f053e3e04507433ab5caa93a8e
Peter Pentchev <pp@storpool.com>
2017-09-01 10:27:02 -0400

    Conditional logging to a file, always to stdout.

--- lib/spcharms/utils.py
+++ lib/spcharms/utils.py
@@ -1,10 +1,15 @@
 import time
 
+from charmhelpers.core import hookenv
+
 def rdebug(s, prefix='storpool'):
 	data = '[{prefix}] {s}'.format(prefix=prefix, s=s)
 	print(data)
 
-	# FIXME: make this conditional, I guess?
-	with open('/tmp/storpool-charms.log', 'a') as f:
-		data_ts = '{tm} {data}'.format(tm=time.ctime(), data=data)
-		print(data_ts, file=f)
+	config = hookenv.config()
+	def_fname = '/dev/null'
+	fname = def_fname if config is None else config.get('storpool_charm_log_file', def_fname)
+	if fname != def_fname:
+		with open(fname, 'a') as f:
+			data_ts = '{tm} {data}'.format(tm=time.ctime(), data=data)
+			print(data_ts, file=f)
