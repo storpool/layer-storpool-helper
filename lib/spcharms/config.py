dca02fe2443452355caf16294d59a316eb1a5cb5
Peter Pentchev <pp@storpool.com>
2017-11-07 17:20:22 -0500

    Store the SP_OURID value into the unit's database.

--- lib/spcharms/config.py
+++ lib/spcharms/config.py
@@ -3,7 +3,11 @@ A StorPool Juju charm helper module for accessing the StorPool configuration.
 """
 import subprocess
 
+from charmhelpers.core import unitdata
+
 cached_config = None
+
+CONFIG_KEY = 'storpool-config.our-id'
 
 
 def get_cached_dict():
@@ -38,3 +42,24 @@ def drop_cache():
     """
     global cached_config
     cached_config = None
+
+
+def get_our_id():
+    """
+    Fetch the cached SP_OURID value from the unit's database.
+    """
+    return unitdata.kv().get(CONFIG_KEY, None)
+
+
+def set_our_id(value):
+    """
+    Store the SP_OURID value into the unit's database.
+    """
+    unitdata.kv().set(CONFIG_KEY, value)
+
+
+def unset_our_id():
+    """
+    Store the SP_OURID value into the unit's database.
+    """
+    unitdata.kv().unset(CONFIG_KEY)
