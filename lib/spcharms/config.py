3b5d1a2f8c4efced187b58acb30f16bab3ad32d3
Peter Pentchev <pp@storpool.com>
2017-11-09 11:18:37 -0500

    Add spcharms.config.set_meta_config() and .m().
    Allow a charm to override the source of StorPool configuration info,
    such as the cinder-storpool charm getting the storpool_version,
    storpool_openstack_version, and storpool_conf variables from
    the storpool-block charm.

--- lib/spcharms/config.py
+++ lib/spcharms/config.py
@@ -3,11 +3,55 @@ A StorPool Juju charm helper module for accessing the StorPool configuration.
 """
 import subprocess
 
-from charmhelpers.core import unitdata
+from charms import reactive
+from charmhelpers.core import hookenv, unitdata
 
 from spcharms import kvdata
 
 cached_config = None
+cached_meta = None
+initializing_config = None
+
+
+class QuasiConfig(object):
+    def r_clear_config(self):
+        global initializing_config
+        saved = initializing_config
+        initializing_config = self
+        self.override = {}
+        self.changed_attrs = {}
+        self.config = {}
+        initializing_config = saved
+
+    def __init__(self):
+        self.r_clear_config()
+
+    def r_set(self, key, value, changed):
+        self.override[key] = value
+        self.changed_attrs[key] = changed
+
+    def get(self, key, default):
+        return self.override.get(key, self.config.get(key, default))
+
+    def changed(self, key):
+        return self.changed_attrs.get(key, False)
+
+    def __getitem__(self, name):
+        # Make sure a KeyError is actually thrown if needed.
+        if name in self.override:
+            return self.override[name]
+        else:
+            return self.config[name]
+
+    def __getattr__(self, name):
+        return self.config.__getattribute__(name)
+
+    def __setattr__(self, name, value):
+        if initializing_config == self:
+            return super(QuasiConfig, self).__setattr__(name, value)
+
+        raise AttributeError('Cannot override the QuasiConfig '
+                             '"{name}" attribute'.format(name=name))
 
 
 def get_cached_dict():
@@ -63,3 +107,46 @@ def unset_our_id():
     Store the SP_OURID value into the unit's database.
     """
     unitdata.kv().unset(kvdata.KEY_OURID)
+
+
+def m():
+    """
+    Get the source of StorPool configuration variables, either hookenv.config()
+    or the data hash specified using set_meta_config().
+    """
+    global cached_meta
+    if cached_meta is None:
+        mm = unitdata.kv().get(kvdata.KEY_META_CONFIG, None)
+        if mm is None:
+            raise Exception('spcharms.config.m() invoked too early!')
+        elif mm == 'None':
+            cached_meta = mm
+        else:
+            cfg = QuasiConfig()
+            for (key, value) in mm.items():
+                cfg.r_set(key, value, True)
+            cached_meta = cfg
+
+    if cached_meta == 'None':
+        return hookenv.config()
+    return cached_meta
+
+
+def set_meta_config(data):
+    """
+    Specify the config dictionary that will provide the StorPool-related
+    charm configuration variables.
+    If specified as None, m() will use hookenv.config().
+    """
+    # Store the new data into the unit's database.
+    if data is None:
+        store = 'None'
+    else:
+        store = data
+    unitdata.kv().set(kvdata.KEY_META_CONFIG, store)
+
+    # Fetch it right back into a QuasiConfig object.
+    global cached_meta
+    cached_meta = None
+    m()
+    reactive.set_state('storpool-helper.config-set')
--- lib/spcharms/kvdata.py
+++ lib/spcharms/kvdata.py
@@ -8,6 +8,7 @@ KEY_OURID = 'storpool-config.our-id'
 KEY_MACHINE_ID = 'storpool-helper.machine-id'
 KEY_PARENT_NODE_ID = 'storpool-helper.parent-node-id'
 KEY_SET_STATES = 'storpool-helper.set-states'
+KEY_META_CONFIG = 'storpool-helper.meta-config'
 
 KEY_LXD_NAME = 'storpool-openstack-integration.lxd-name'
 
